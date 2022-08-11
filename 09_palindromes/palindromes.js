3bd7055abb341539dd1667d777e369a1c1dcada7
jabrij <jabrijuhinin93@gmail.com>
2022-08-11 15:43:13 +0800

    Finish javascript exercise 09 - palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    // Step 1. Lowercase the string and use the RegExp to remove unwanted characters from it
+    let re = /[^A-Za-z0-9]/g;
+
+    let lowRegExp = string.toLowerCase().replace(re, '');
+    // str.toLowerCase() = "A man, a plan, a canal. Panama".toLowerCase() = "a man, a plan, a canal. panama"
+    // str.replace(/[\W_]/g, '') = "a man, a plan, a canal. panama".replace(/[\W_]/g, '') = "amanaplanacanalpanama"
+    // var lowRegStr = "amanaplanacanalpanama";
+
+    // Step 2. Use the same chaining methods with built-in functions from the previous article 'Three Ways to Reverse a String in JavaScript'
+    let reverseString = lowRegExp.split('').reverse().join('');
+    // lowRegStr.split('') = "amanaplanacanalpanama".split('') = ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"]
+    // ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"].reverse() = ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"]
+    // ["a", "m", "a", "n", "a", "p", "l", "a", "n", "a", "c", "a", "n", "a", "l", "p", "a", "n", "a", "m", "a"].join('') = "amanaplanacanalpanama"
+    // So, "amanaplanacanalpanama".split('').reverse().join('') = "amanaplanacanalpanama";
+    // And, var reverseStr = "amanaplanacanalpanama";
+
+    // Step 3. Check if reverseStr is strictly equals to lowRegStr and return a Boolean
+    return reverseString === lowRegExp;
 };
 
 // Do not edit below this line
