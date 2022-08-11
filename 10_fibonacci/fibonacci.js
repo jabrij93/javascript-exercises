4e39bec3764d9db1aaf9e4a572f2cf82c92779a6
jabrij <jabrijuhinin93@gmail.com>
2022-08-11 15:44:12 +0800

    Finish javascript exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let a = 0; 
+    let b = 1; 
+    let temp;
 
-};
+    if(number < 0) {
+        return "OOPS";
+    }
+      
+    while (number >= 0){
+        temp = a;
+        a = a + b;
+        b = temp;
+        number--;  
+    }
+    return b;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
